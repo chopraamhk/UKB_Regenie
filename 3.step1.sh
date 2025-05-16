361fb82ed140436b731615692f4a32122104eecb
Mehak Chopra <m.chopra1@universityofgalway.ie>
2025-05-16 13:10:38 +0100

    Update 3.step1.sh

--- 3.step1.sh
+++ 3.step1.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-#
-#SBATCH --job-name="s1"
-#SBATCH -o s1.o%j
-#SBATCH -e s1.e%j
-#SBATCH --mail-user=<>
-#SBATCH --mail-type=ALL
+#SBATCH --job-name="regenie_chr"
+#SBATCH -o regenie_chr_%A_%a.out
+#SBATCH -e regenie_chr_%A_%a.err
+#SBATCH --array=1-22
 #SBATCH --partition="highmem","normal"
+#SBATCH --mail-user=m.chopra1@universityofgalway.ie
+#SBATCH --mail-type=ALL
 
 module load Anaconda3/2024.02-1
-conda activate regenie_env
+conda activate regenie
 
-regenie --step 1 --force-step1
-        --bed c1_22_regenie 
-        --extract qc/qc_pass.snplist 
-        --keep qc/qc_pass.id 
-        --phenoFile ../../phenotypes/norm_pheno_aao.txt 
-        --covarFile ../../output/after_pca_wbi/makeCovar/qcovars7.txt 
-        --qt --bsize 1000 --lowmem --lowmem-prefix tmpdir/regenie_tmp_preds 
-        --out step1/test_step1_B
+CHR=$SLURM_ARRAY_TASK_ID
+
+regenie --step 1 \
+  --bed /data3/mchopra/ukb_genotype_mri_passed/prune_wbi_icd10/ld_${CHR} \
+  --extract /data3/mchopra/ukb_genotype_mri_passed/prune_wbi_icd10/mac_10/snps_pass_${CHR}.snplist \
+  --phenoFile ../../../../../phenotypes/norm_pheno_aao.txt \
+  --covarFile ../../bolt_covars.txt \
+  --qt --bsize 1000 --lowmem \
+  --lowmem-prefix ../../tmpdir/regenie_${CHR}_tmp \
+  --out s1_mac10_${CHR}_age
+
